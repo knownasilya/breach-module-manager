a0c9e7e8904bf06695539d4c6f2a4fd79820a0b3
Ilya Radchenko <ilya@burstcreations.com>
2014-07-15 08:47:59 -0400

    Move search out of refresh

--- index.js
+++ index.js
@@ -15,18 +15,17 @@ var _ = require('lodash');
 var cache = require('./lib/cache');
 var initApp = require('./lib/app');
 var out = common.log.out;
+var error = common.log.error;
 
 function bootstrap(server) {
   var port = server.address().port;
 
   breach.init(function () {
     breach.expose('init', function (src, args, cb) {
-      cache.refresh().then(function () {
-        cache.search('manager').then(function (result) {
-          var moduleNames = _.map(result, function (pkg) { return pkg.name + '@' + pkg['dist-tags'].latest; });
-          out('Found: ' + moduleNames.join(', '));
-          cb();
-        });
+      cache.search('manager').then(function (result) {
+        var moduleNames = _.map(result, function (pkg) { return pkg.name + '@' + pkg['dist-tags'].latest; });
+        out('Found: ' + moduleNames.join(', '));
+        cb();
       });
 
       breach.module('core').call('tabs_new_tab_url', { 
@@ -39,7 +38,7 @@ function bootstrap(server) {
         out('New tab page initialized: http://127.0.0.1:' + port + '/newtab');
       });
 
-      out('Wait for result');
+      out('Waiting for result..');
     });
     breach.expose('kill', function (args, cb) {
       common.exit(0);
