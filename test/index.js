41f0baf097ef7fbcfcb3d81f57c215cc466ed273
Ilya Radchenko <ilya@burstcreations.com>
2014-07-17 23:56:57 -0400

    Swap tape for prova
    Prova uses tape, but also makes it better to test in browser
    if needed, and gives you better errors.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var test = require('tape');
+var test = require('prova');
 
 test('Unit', require('./unit'));
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var test = require('tape');
-
 module.exports = function (t) {
   t.test('Categories', require('./categories'));
 };
