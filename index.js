653c462812d606c203ea26d221072677101e8031
Michael Bøcker-Larsen <mblarsen@gmail.com>
2014-07-24 12:01:20 +0545

    Added npm-local-cache dependency

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
  */
 'use strict';
 
+var moduleKeyword = 'breach-module';
+
 var breach = require('breach_module');
 var socket = require('socket.io');
 var async = require('async');
@@ -15,7 +17,7 @@ var normalize = require('npm-normalize');
 var common = require('breach_module/lib/common');
 var _ = require('lodash');
 var es = require('event-stream');
-var cache = require('./lib/cache');
+var cache = require('npm-local-cache')({ keywords: [moduleKeyword] });
 var initApp = require('./lib/app');
 var categories = require('./lib/categories');
 var out = common.log.out;
@@ -31,7 +33,8 @@ function bootstrap(server) {
         socket.on('handshake', function (handshakeId) {
           out('Connected to `' + handshakeId + '` via websocket.');
           // TODO: stream the modules by page, sending a page (~25 items, maybe configurable later) at a time
-          cache.all(function (data) {
+          cache.init().then(function (data) {
+            var data = cache.getPackages();
             if (typeof data === 'object') {
               var normalized = normalize(data);
               socket.emit('modules-single', { name: normalized.name, version: normalized.version, selected: false });
@@ -46,11 +49,12 @@ function bootstrap(server) {
         });
       }); 
 
-      cache.all().then(function (result) {
+      cache.init().then(function (result) {
+        var result = cache.getPackages();
         var moduleNames = _.map(result, function (pkg) { return pkg.name + '@' + pkg['dist-tags'].latest; });
 
         out('Found: (' + moduleNames.length + ') ' + moduleNames.join(', '));
-        out('Categories: ' + categories(result, 10, [cache.moduleKeyword]));
+        out('Categories: ' + categories(result, 10, [moduleKeyword]));
         cb();
       });
 
@@ -90,7 +94,7 @@ function normalizeResult(result) {
       };
     });
 
-    var cats = categories(result, 5, [cache.moduleKeyword]).map(function (catName) {
+    var cats = categories(result, 5, [moduleKeyword]).map(function (catName) {
       return { name: catName };
     });
 
